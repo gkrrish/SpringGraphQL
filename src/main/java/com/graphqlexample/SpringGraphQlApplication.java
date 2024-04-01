ae98f56f0b0da307fa1362dc305ccb2e4d32850f
Krishna <gkrrish.11@gmail.com>
2024-04-01 10:39:14 +0530

    changes done for testing the GraphQL

--- src/main/java/com/graphqlexample/SpringGraphQlApplication.java
+++ src/main/java/com/graphqlexample/SpringGraphQlApplication.java
@@ -2,8 +2,10 @@ package com.graphqlexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.graphqlexample", "com.graphqlexample.component", "com.graphqlexample.repository"})
 public class SpringGraphQlApplication {
 
 	public static void main(String[] args) {
